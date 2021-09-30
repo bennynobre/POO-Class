d6b63d75a50f88cade3050d3876a412f25c27ca2
Benny Nobre <bennynobre1@gmail.com>
2021-09-30 13:30:00 -0300

    Adding class activity update

--- Class1/EP2/AtividadeEstressados.cpp
+++ Class1/EP2/AtividadeEstressados.cpp
@@ -8,10 +8,37 @@ bool existe (std::vector<int> vet, int valor){
     return false;
 }
 
+int procurar_valor(std::vector<int> vet, int valor) {
+    for (int i = 0; i < (int)vet.size(); i++)
+        if(vet[i] == valor)
+            return i;
+    return -1;
+}
+
+int contar(std::vector<int> vet, int valor){
+    int cont = 0;
+    for (int i = 0; i < (int)vet.size(); i++)
+        if(vet[i] == valor){
+            cont++;
+        }
+    return cont;
+   
+}
+
+int procurar_valor_apos(std::vector<int> vet, int valor){
+    for (int i = valor; i < (int)vet.size(); i++)
+        if(vet[i] == valor)
+            return i;
+    return -1;
+   
+}
+
 int main()
 {
     std::cout << existe({1, 2, 3, 4, 5}, 0) << '\n';
-    std::cout << existe({1, 2, 3, 4, 5}, 3) << '\n';
+    std::cout << procurar_valor({1, 2, 3, 4, 5}, 3) << '\n';
+    std::cout << contar({1, 4, 3, 4, 4}, 4) << '\n';
+    std::cout << contar({1, 4, 3, 5, 4}, 4) << '\n';
 
     return 0;
 }
