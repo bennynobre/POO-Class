d59c01cda79c2fff49dfcc2dba441da319cdb27a
Benny <bennynobre1@gmail.com>
2021-10-10 23:11:32 -0300

    Add a new question and a new project using SFML

---
+++ Class1/EP3/Filter.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<iterator>
+
+using namespace std;
+
+int clone(const vector<int>& vet){
+
+    
+
+}
+
+
+int main(){
+
+    vector<int> vet = {5, 3, -1, -50, -1, -99};
+    int P = 0;
+
+    cout << clone(vet) << '\n';
+
+
+    return 0;
+}
