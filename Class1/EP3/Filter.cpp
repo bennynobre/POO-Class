30d9245cc7a44f323a5157c0eccbf79ed0a0d900
Benny <bennynobre1@gmail.com>
2021-10-11 11:54:44 -0300

    Add a new classwork about Filter in C++

--- Class1/EP3/Filter.cpp
+++ Class1/EP3/Filter.cpp
@@ -5,19 +5,92 @@
 
 using namespace std;
 
-int clone(const vector<int>& vet){
+vector<int> pegar_mulheres_calmas(const vector<int>& vet) {
 
+    vector<int> mulheres {};
     
+    for (int i=0; i < (int) vet.size() ; i++) {
+
+        if(vet[i] < 0 and vet[i] >= -10){
+        mulheres.push_back(vet[i] * -1);
+        }
+    }
+
+    return mulheres;
+}
+
+
+vector<int> pegar_calmos(const vector<int>& vet) {
+
+     vector<int> calmos {};
+    
+    for (int i=0; i < (int) vet.size() ; i++) {
+
+        if (vet[i] > 0 and vet[i] <= 10){
+        calmos.push_back(vet[i]);
+        }
+        else if (vet[i] < 0 and vet[i] >= -10) {
+            calmos.push_back(vet[i] * -1);
+        }
+    }
+
+    return calmos;
+}
+
+
+vector<int> pegar_homens(const vector<int>& vet) {
+
+    vector<int> homens {};
+    
+    for (int i=0; i < (int) vet.size() ; i++) {
+
+        if(vet[i] > 0){
+        homens.push_back(vet[i]);
+        }
+    }
+
+    return homens;
+
+}
+
+vector <int> clone(const vector<int>& vet) {
+
+    vector<int> vet2 {};
+
+    for (int i=0; i < (int) vet.size() ; i++) {
+        vet2.push_back(vet[i]);
+    }
+
+    return vet2;
 
 }
 
 
 int main(){
 
-    vector<int> vet = {5, 3, -1, -50, -1, -99};
-    int P = 0;
+    vector<int> vet = {5, 3, -1, -50, -5, -99};
 
-    cout << clone(vet) << '\n';
+    for( int i : clone( vet )){
+        cout << i << " ";
+    }
+
+    cout << '\n';
+
+    for( int i : pegar_homens( vet )){
+        cout << i << " ";
+    }
+
+    cout << '\n';
+
+    for( int i : pegar_calmos( vet )){
+        cout << i << " ";
+    }
+
+    cout << '\n';
+
+    for( int i : pegar_mulheres_calmas( vet )){
+        cout << i << " ";
+    }
 
 
     return 0;
